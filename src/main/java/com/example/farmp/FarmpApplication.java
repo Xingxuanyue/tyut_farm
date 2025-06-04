e7df792929e4931fc26c1c9f58ad5d5efee6788b
Xingxuanyue <1794191294@qq.com>
2025-06-04 19:32:34 +0800

    初始化项目

---
+++ src/main/java/com/example/farmp/FarmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farmp/FarmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
