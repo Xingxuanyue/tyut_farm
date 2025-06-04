6346f525d20bd77be51449ea4d2072370d2f51af
Xingxuanyue <1794191294@qq.com>
2025-06-04 18:23:22 +0800

    后端搭建111

---
+++ src/main/java/com/example/farm111/Farm111Application.java
@@ -0,0 +1,13 @@
+package com.example.farm111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Farm111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farm111/Farm111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farm111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farm111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
