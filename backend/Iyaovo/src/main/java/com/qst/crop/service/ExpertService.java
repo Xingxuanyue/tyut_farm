07eb2b08d9fd6532bf9f9399da43225719866423
iYaovo <2293257241@qq.com>
2025-06-11 15:14:23 +0800

    feat:编写核心功能的接口和服务类

---
+++ backend/Iyaovo/src/main/java/com/qst/crop/service/ExpertService.java
@@ -0,0 +1,24 @@
+package com.qst.crop.service;
+
+import com.github.pagehelper.PageInfo;
+import com.qst.crop.entity.Expert;
+
+import java.util.List;
+
+public interface ExpertService {
+
+    void delete(String type);
+
+    void insert(Expert record);
+
+    Expert selectById(String type);
+
+    void updateById(Expert record);
+
+    List<Expert> selectAllExpert();
+
+    PageInfo<Expert> findPage(Integer pageNum);
+
+    PageInfo<Expert> findPageByKeys(String keys, Integer pageNum);
+
+}
